58f344cdb341958b6230f77d539641e00e50991c
muhnizar <mooh.nizar@yahoo.com>
2016-01-13 10:30:26 +0700

    creating listview layout

--- app/src/main/java/com/example/moohn/listviewtutorial/MyListActivity.java
+++ app/src/main/java/com/example/moohn/listviewtutorial/MyListActivity.java
@@ -2,11 +2,28 @@ package com.example.moohn.listviewtutorial;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MyListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                R.layout.rowlayout, R.id.label, values);
+
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
     }
 }
